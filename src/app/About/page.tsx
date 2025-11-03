3775541dc68426afd4846e46e9b4803f49cb1142
nazi sharifi <banonazisharifi@gmail.com>
2025-11-03 20:48:25 +0430

    first commit

---
+++ src/app/About/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='flex justify-center items-center h-screen w-full'>
+    <div className='w-[40%] h-[40%] bg-black font-serif flex justify-center text-center items-center text-7xl font-bold'>
+        <h1 className='bg-linear-270 from-blue-500 animate-pulse to-yellow-600  bg-clip-text text-transparent'>Welcome to About Us Page....</h1>
+    </div>
+    </div>
+  )
+}
+
+export default Page
